d81a92ff9173c55930bc15c2ab1d8fe7e447af50
weiqiuxia <weiqiuxia@seekgene.com>
2026-07-01 13:35:16 +0800

    add: add taps parameter

--- ALLCools/__main__.py
+++ ALLCools/__main__.py
@@ -218,7 +218,7 @@ def bam_to_allc_register_subparser(subparser):
                                  "to store mpileup output for debugging purposes.")
     parser.add_argument("--taps",
                         action="store_true", 
-                        help = "TAPS converted data, convert C to T and vice versa."
+                        help = "TAPS converted data, convert C to T and vice versa.")
     parser.set_defaults(debug=False)
     parser.add_argument(
         "--save_count_df",
